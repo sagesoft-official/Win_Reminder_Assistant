811b29c0fbfd18810d07c518cfce8077615e0a47
狐日泽 <shdocter@nya-wsl.com>
2023-12-07 00:30:10 +0800

    v1.3.0
    - 基于Tkinter的GUI
    - 移除上次提醒时间
    - 老版本源代码 -> wra_v1.2.0.py

--- wra.py
+++ wra.py
@@ -1,79 +1,330 @@
 '''
 Author: Nya-WSL
 Copyright © 2023 by Nya-WSL All Rights Reserved. 
-Date: 2023-12-04 00:01:16
+Date: 2023-12-06 21:56:21
 LastEditors: 狐日泽
-LastEditTime: 2023-12-06 02:03:08
+LastEditTime: 2023-12-07 00:24:22
 '''
-
-import sys
 import time
 import datetime
-import PySimpleGUI as sg
 from win11toast import toast
+import tkinter.messagebox as tkmb
+from tkinter import Tk, Canvas, Entry, Button, PhotoImage
 
-version = "1.2.1"
+# OUTPUT_PATH = Path(__file__).parent
+# ASSETS_PATH = OUTPUT_PATH / Path("assets/frame0")
 
-layout = [[sg.Text("时（必须为>=1的整数，留空默认为0）")],
-        [sg.Input(key='-TimeHours-')],
-        [sg.Text("分（必须为>=1的整数，留空默认为0）")],
-        [sg.Input(key='-TimeMinutes-')],
-        [sg.Text("秒（必须为>=0的整数，留空默认为0）")],
-        [sg.Input(key='-TimeSeconds-')],
-        [sg.Text("通知标题")],
-        [sg.Input(key='-title-')],
-        [sg.Text("通知内容")],
-        [sg.Input(key='-launch-')],
-        [sg.Text("通知场景")],
-        [sg.Text("reminder[默认]，alarm[无效]，incomingCall，urgent")],
-        [sg.Input(key='-scenario-')],
-        [sg.Text("通知时间（long，short，默认short）")],
-        [sg.Input(key='-duration-')],
-        [sg.Text("", size=(40,1), key='-OUTPUT-')],
-        [sg.Button('确定')]]
+# def relative_to_assets(path: str) -> Path:
+#     return ASSETS_PATH / Path(path)
 
-window = sg.Window(f'Windows提醒小助手v{version}', layout=layout)
+def relative_to_assets(path: str):
+    return "assets/frame0/" + path
 
-while True:
-    event, values = window.read()
-
-    if event == sg.WINDOW_CLOSED or event == 'Quit':
-        break
-
+def wra():
     TimeLeft = 0
-    if values['-TimeHours-'] == "" and values['-TimeMinutes-'] == "" and values['-TimeSeconds-'] == "":
-        ErrorPopup = sg.popup("倒计时不该为空！")
-        if ErrorPopup == None or ErrorPopup == "OK":
-            pass
+    TimeHours = entry_1.get()
+    TimeMinutes = entry_2.get()
+    TimeSeconds = entry_3.get()
+    Launch = entry_4.get()
+    scenario = entry_5.get()
+    duration = entry_6.get()
+    Title = entry_7.get()
+    print(f"1{TimeHours}, 2{TimeMinutes}, 3{TimeSeconds}, 4{Title}, 5{Launch}, 6{scenario}, 7{duration}")
+    
+    if TimeHours == "" and TimeMinutes == "" and TimeSeconds == "":
+        TimeError = tkmb.showerror(title="参数错误", message="倒计时不该为空！")
+        if TimeError:
+            return
+    if TimeHours == "":
+        TimeHours = 0
+    if TimeMinutes == "":
+        TimeMinutes = 0
+    if TimeSeconds == "":
+        TimeSeconds = 0
+    if scenario == "":
+        scenario = 'reminder'
+    if duration == "":
+        duration = 'short'
+    TimeLeft = (int(TimeHours) * 3600) + (int(TimeMinutes) * 60) + int(TimeSeconds)
+    StartTimeLeft = tkmb.askyesno(
+            "SageSoft | Nya-WSL",
+            "单击Yes开始计时，单击No取消计时\n"
+            f"预计时间：{TimeLeft}秒")
+    if not StartTimeLeft:
+        return
+    while int(TimeLeft) > 0:
+        print(TimeLeft) # debug
+        time.sleep(1)
+        TimeLeft = int(TimeLeft) - 1
+        LeftText = canvas.create_text(
+            175.0,
+            273.0,
+            anchor="nw",
+            text=f"倒计时：{TimeLeft}",
+            fill="#000000",
+            font=("Inter", 12 * -1)
+            )
+        window.update()
+        canvas.delete(LeftText)
     else:
-        if values['-TimeHours-'] == "":
-            values['-TimeHours-'] = 0
-        if values['-TimeMinutes-'] == "":
-            values['-TimeMinutes-'] = 0
-        if values['-TimeSeconds-'] == "":
-            values['-TimeSeconds-'] = 0
+        global LastTimeText
+        toast(Title, Launch, scenario=scenario, duration=duration)
 
-        TimeLeft = (int(values['-TimeHours-']) * 3600) + (int(values['-TimeMinutes-']) * 60) + int(values['-TimeSeconds-'])
-        StartTimeLeft = sg.popup_yes_no(f"单击Yes开始计时，单击No取消计时，预计时间：{TimeLeft}秒", title="是否开始倒计时？")
-        if StartTimeLeft == None or StartTimeLeft == "No":
-            pass
-        else:
-            if values['-scenario-'] == "":
-                values['-scenario-'] = 'reminder'
-            if values['-duration-'] == "":
-                values['-duration-'] = 'short'
 
-            while int(TimeLeft) > 0:
-                if event == sg.WINDOW_CLOSED or event == 'Quit':
-                    sys.exit()
-                window.refresh()
-                print(TimeLeft) # debug
-                time.sleep(1)
-                TimeLeft = int(TimeLeft) - 1
-                window.refresh()
-                window['-OUTPUT-'].update(f"倒计时：{TimeLeft}")
-            else:
-                toast(values['-title-'], values['-launch-'], scenario=values['-scenario-'], duration=values['-duration-'])
-                window['-OUTPUT-'].update(f"上次提醒时间：{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+version = "1.3.0"
+window = Tk()
+window.iconbitmap("icon.ico")
+window.title(f"Windows提醒小助手v{version} By. SageSoft")
+window.geometry("400x350")
+window.configure(bg = "#D6EEFF")
 
-window.close()
+canvas = Canvas(
+    window,
+    bg = "#D6EEFF",
+    height = 350,
+    width = 400,
+    bd = 0,
+    highlightthickness = 0,
+    relief = "ridge"
+)
+
+canvas.place(x = 0, y = 0)
+canvas.create_text(
+    82.0,
+    13.0,
+    anchor="nw",
+    text="时",
+    fill="#000000",
+    font=("Inter", 12 * -1)
+)
+
+entry_image_1 = PhotoImage(
+    file=relative_to_assets("entry_1.png"))
+entry_bg_1 = canvas.create_image(
+    124.0,
+    21.0,
+    image=entry_image_1
+)
+entry_1 = Entry(
+    bd=0,
+    bg="#FFFFFF",
+    fg="#000716",
+    highlightthickness=0
+)
+entry_1.place(
+    x=110.0,
+    y=11.0,
+    width=28.0,
+    height=18.0
+)
+
+canvas.create_text(
+    167.0,
+    13.0,
+    anchor="nw",
+    text="分",
+    fill="#000000",
+    font=("Inter", 12 * -1)
+)
+
+entry_image_2 = PhotoImage(
+    file=relative_to_assets("entry_2.png"))
+entry_bg_2 = canvas.create_image(
+    211.0,
+    21.0,
+    image=entry_image_2
+)
+entry_2 = Entry(
+    bd=0,
+    bg="#FFFFFF",
+    fg="#000716",
+    highlightthickness=0
+)
+entry_2.place(
+    x=197.0,
+    y=11.0,
+    width=28.0,
+    height=18.0
+)
+
+canvas.create_text(
+    252.0,
+    13.0,
+    anchor="nw",
+    text="秒",
+    fill="#000000",
+    font=("Inter", 12 * -1)
+)
+
+entry_image_3 = PhotoImage(
+    file=relative_to_assets("entry_3.png"))
+entry_bg_3 = canvas.create_image(
+    294.0,
+    21.0,
+    image=entry_image_3
+)
+entry_3 = Entry(
+    bd=0,
+    bg="#FFFFFF",
+    fg="#000716",
+    highlightthickness=0
+)
+entry_3.place(
+    x=280.0,
+    y=11.0,
+    width=28.0,
+    height=18.0
+)
+
+canvas.create_text(
+    176.0,
+    41.0,
+    anchor="nw",
+    text="通知标题",
+    fill="#000000",
+    font=("Inter", 12 * -1)
+)
+
+entry_image_4 = PhotoImage(
+    file=relative_to_assets("entry_4.png"))
+entry_bg_4 = canvas.create_image(
+    200.0,
+    122.0,
+    image=entry_image_4
+)
+entry_4 = Entry(
+    bd=0,
+    bg="#FFFFFF",
+    fg="#000716",
+    highlightthickness=0
+)
+entry_4.place(
+    x=135.0,
+    y=112.0,
+    width=130.0,
+    height=18.0
+)
+
+canvas.create_text(
+    176.0,
+    92.0,
+    anchor="nw",
+    text="通知内容",
+    fill="#000000",
+    font=("Inter", 12 * -1)
+)
+
+entry_image_5 = PhotoImage(
+    file=relative_to_assets("entry_5.png"))
+entry_bg_5 = canvas.create_image(
+    199.0,
+    191.0,
+    image=entry_image_5
+)
+entry_5 = Entry(
+    bd=0,
+    bg="#FFFFFF",
+    fg="#000716",
+    highlightthickness=0
+)
+entry_5.place(
+    x=134.0,
+    y=181.0,
+    width=130.0,
+    height=18.0
+)
+
+canvas.create_text(
+    176.0,
+    140.0,
+    anchor="nw",
+    text="通知场景",
+    fill="#000000",
+    font=("Inter", 12 * -1)
+)
+
+canvas.create_text(
+    56.0,
+    160.0,
+    anchor="nw",
+    text="reminder[默认]，alarm[无效]，incomingCall，urgent",
+    fill="#000000",
+    font=("Inter", 12 * -1)
+)
+
+entry_image_6 = PhotoImage(
+    file=relative_to_assets("entry_6.png"))
+entry_bg_6 = canvas.create_image(
+    199.0,
+    255.0,
+    image=entry_image_6
+)
+entry_6 = Entry(
+    bd=0,
+    bg="#FFFFFF",
+    fg="#000716",
+    highlightthickness=0
+)
+entry_6.place(
+    x=134.0,
+    y=245.0,
+    width=130.0,
+    height=18.0
+)
+
+canvas.create_text(
+    163.0,
+    209.0,
+    anchor="nw",
+    text="通知留存时间",
+    fill="#000000",
+    font=("Inter", 12 * -1)
+)
+
+canvas.create_text(
+    153.0,
+    224.0,
+    anchor="nw",
+    text="long，short[默认]",
+    fill="#000000",
+    font=("Inter", 12 * -1)
+)
+
+entry_image_7 = PhotoImage(
+    file=relative_to_assets("entry_7.png"))
+entry_bg_7 = canvas.create_image(
+    200.0,
+    74.0,
+    image=entry_image_7
+)
+entry_7 = Entry(
+    bd=0,
+    bg="#FFFFFF",
+    fg="#000716",
+    highlightthickness=0
+)
+entry_7.place(
+    x=135.0,
+    y=64.0,
+    width=130.0,
+    height=18.0
+)
+
+button_image_1 = PhotoImage(
+    file=relative_to_assets("button_1.png"))
+button_1 = Button(
+    image=button_image_1,
+    borderwidth=0,
+    highlightthickness=0,
+    command=wra,
+    relief="flat"
+)
+button_1.place(
+    x=164.0,
+    y=323.0,
+    width=72.0,
+    height=20.0
+)
+
+window.resizable(False, False)
+window.mainloop()
